cacb4dd59af690a09e82f80a9fd6ff9fd0f3715f
admin <qang.wallace@gmail.com>
2023-10-21 13:26:35 +0800

    修复关闭按钮报错的问题

--- src/views/service/deploy.vue
+++ src/views/service/deploy.vue
@@ -351,6 +351,7 @@ export default {
     },
     async closeClick(row, item){
       let data = {
+        id: row.id,
         svc_name: row.svc_name,
         docker_port: row.docker_port,
         opt_type: 'stop',
